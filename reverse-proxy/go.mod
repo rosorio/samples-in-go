386acc0205603096d359c4a9b60e5942cfddcc3a
Rodrigo OSORIO <rodrigo.osorio@softathome.com>
2025-04-01 11:01:07 +0200

    Use flag to pass arguments, redo formating.

---
+++ reverse-proxy/go.mod
@@ -0,0 +1,3 @@
+module example.com/rev-proxy
+
+go 1.21.13
--- reverse-proxy/rev-proxy.go
+++ reverse-proxy/rev-proxy.go
@@ -6,31 +6,37 @@
 package main
 
 import(
-        "log"
-        "net/url"
-        "net/http"
-        "net/http/httputil"
+    "flag"
+    "log"
+    "net/url"
+    "net/http"
+    "net/http/httputil"
 )
 
-func main() {
-        remote, err := url.Parse("http://google.com")
-        if err != nil {
-                panic(err)
-        }
 
-        handler := func(p *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
-                return func(w http.ResponseWriter, r *http.Request) {
-                        log.Println(r.URL)
-                        r.Host = remote.Host
-                        w.Header().Set("X-Ben", "Rad")
-                        p.ServeHTTP(w, r)
-                }
-        }
+func main() {
+    var rurl = flag.String("remote", "", "Remote URL")
+    var lbind = flag.String("local", "", "Bind address")
+    flag.Parse()
+
+    remote, err := url.Parse(*rurl)
+    if err != nil {
+            panic(err)
+    }
+
+    handler := func(p *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
+        return func(w http.ResponseWriter, r *http.Request) {
+                log.Println(r.URL)
+                r.Host = remote.Host
+                w.Header().Set("X-Ben", "Rad")
+                p.ServeHTTP(w, r)
+            }
+    }
         
-        proxy := httputil.NewSingleHostReverseProxy(remote)
-        http.HandleFunc("/", handler(proxy))
-        err = http.ListenAndServe(":8080", nil)
-        if err != nil {
-                panic(err)
-        }
+    proxy := httputil.NewSingleHostReverseProxy(remote)
+    http.HandleFunc("/", handler(proxy))
+    err = http.ListenAndServe(*lbind, nil)
+    if err != nil {
+        panic(err)
+    }
 }
